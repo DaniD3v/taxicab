beef92ccf44d4cb510274750c5b049e305b3e4cf
DaniD3v <sch220233@spengergasse.at>
2023-10-18 12:15:53 +0200

    performance increase
    * using Vec and HashSet to skip useless continue branches
    * put lower_bound at the start of the method signature
    * improved performance tracking code

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,35 @@
 use std::collections::HashSet;
 
-fn generate_cube_hashset(upper_bound: u128) -> HashSet<u128> {
-    let mut table = HashSet::new();
+fn generate_cube_table(upper_bound: u128) -> Vec<u128> {
+    let mut table = Vec::new();
 
     for i in 0u128.. {
         let result = i.pow(3);
         if result > upper_bound {break;}
 
-        table.insert(result);
+        table.push(result);
     }
 
     table
 }
 
-fn bruteforce_taxicab(needed_matches: u8, upper_bound: u128, lower_bound: u128) {
-    let table = generate_cube_hashset(upper_bound);
-    println!("Iterations needed per number: {}", table.len());
+fn bruteforce_taxicab(needed_matches: u8, lower_bound: u128, upper_bound: u128) {
+    let table_ordered = generate_cube_table(upper_bound);
+    let table_set: HashSet<u128> = table_ordered.clone().into_iter().collect();
 
-    let start = std::time::Instant::now();
+    println!("Iterations needed per number: {}", table_ordered.len());
+    let mut start = std::time::Instant::now();
 
-    for i in lower_bound..lower_bound + 10000 {
-        if i % 10000 == 0 {println!("Checking {i}");}
+    for i in lower_bound..upper_bound {
         let mut matches = 0;
+        if i % 1000000 == 0 {
+            println!("Checking {i} in {:#?}", start.elapsed());
+            start = std::time::Instant::now();
+        }
 
-        for tn in table.iter() {
-            if tn > &i { continue; }
-            if table.contains(&(i - tn)) {matches += 1;}
+        for tn in table_ordered.iter() {
+            if tn > &i { break; }
+            if table_set.contains(&(i - tn)) {matches += 1;}
         }
 
         matches /= 2;
@@ -33,10 +37,8 @@ fn bruteforce_taxicab(needed_matches: u8, upper_bound: u128, lower_bound: u128) 
             println!("{i} has {matches} matches!")
         }
     }
-
-    println!("{:#?} elapsed", start.elapsed());
 }
 
 fn main() {
-    bruteforce_taxicab(4, 6963472309248, 87539319);
+    bruteforce_taxicab(4, 87539319, 6963472309248);
 }
