5fa87341958b6315ce9015522a6f57a89e53da74
DaniD3v <sch220233@spengergasse.at>
2023-10-19 17:51:43 +0200

    renamed smart to dumb and created file for new smart algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+mod table_gen;
 mod naive;
 mod smart;
-mod table_gen;
+mod dumb;
 
 fn main() {
-    smart::search_taxicab(4, 6963472309248); // doable
+    // dumb::search_taxicab(3, 87539319);
+    dumb::search_taxicab(4, 6963472309248); // doable
 
     // smart::search_taxicab(5, 48988659276962496) // if you have 3TB of ram and 180 minutes ig
     // naive::bruteforce_taxicab(4, 87539319, 6963472309248); // way too slow
