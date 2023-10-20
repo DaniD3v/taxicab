7d39ad6c086c6408d090310149a2de2737161701
DaniD3v <sch220233@spengergasse.at>
2023-10-20 12:40:29 +0200

    New smart algorithm with O(N) space complexity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate core;
+
 mod table_gen;
 mod naive;
 mod smart;
@@ -5,8 +7,11 @@ mod dumb;
 
 fn main() {
     // dumb::search_taxicab(3, 87539319);
-    dumb::search_taxicab(4, 6963472309248); // doable
+    // dumb::search_taxicab(4, 6963472309248); // doable
 
-    // smart::search_taxicab(5, 48988659276962496) // if you have 3TB of ram and 180 minutes ig
+    // smart::search_taxicab(2, 87539319);
+    // smart::search_taxicab(4, 6963472309248);
+    smart::search_taxicab(5, 48988659276962496)
+
     // naive::bruteforce_taxicab(4, 87539319, 6963472309248); // way too slow
 }
---
+++ src/smart.rs
@@ -0,0 +1,75 @@
+use crate::table_gen::generate_cube_table;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashSet};
+
+#[derive(Eq, PartialEq)]
+struct Number {
+    sum: u128,
+    x: usize,
+    y: usize
+}
+
+impl Number {
+    pub fn new(x: usize, y: usize, table: &Vec<u128>) -> Self {
+        Self { sum: table[x] + table[y], x, y }
+    }
+}
+
+impl Ord for Number {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.sum.cmp(&other.sum).reverse()
+    }
+}
+
+impl PartialOrd for Number {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+// doesn't terminate at upper_bound!
+pub fn search_taxicab(matches_needed: u8, upper_bound: u128) {
+    let table = generate_cube_table(upper_bound);
+
+    let mut queue = BinaryHeap::new();
+    queue.push(Number::new(1, 1, &table));
+
+    let mut cols: HashSet<usize> = HashSet::new(); // x
+    let mut rows: HashSet<usize> = HashSet::new(); // y
+
+    let mut previous = 0;
+    let mut matches_count = 1;
+
+    while let Some(Number {sum, x, y}) = queue.pop() {
+        debug_assert!(sum >= previous);
+        if sum % 100000000000 == 0 {println!("{:.2}% there!", (sum as f64/upper_bound as f64)*100.0);} // this is mathematically very incorrect
+
+        if sum == previous {
+            matches_count += 1;
+            if matches_count >= matches_needed {println!("{sum} has {matches_count} matches");}
+        } else {
+            matches_count = 1;
+            previous = sum;
+        }
+
+        cols.remove(&x);
+        rows.remove(&y);
+
+        let adjacent = x+1;
+        if adjacent < table.len() && !cols.contains(&adjacent) {
+            insert(&mut queue, &mut cols, &mut rows, &table, adjacent, y);
+        }
+
+        let below = y+1;
+        if below <= x && below < table.len() && !rows.contains(&below) {
+            insert(&mut queue, &mut cols, &mut rows, &table, x, below);
+        }
+    }
+}
+
+fn insert(queue: &mut BinaryHeap<Number>, cols: &mut HashSet<usize>, rows: &mut HashSet<usize>, table: &Vec<u128>, x: usize, y: usize) {
+    queue.push(Number::new(x, y, &table));
+
+    cols.insert(x);
+    rows.insert(y);
+}
